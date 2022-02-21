f3298927df86779b6264cffa2d38678755f8f664
Mohamed Zabara <mohamedmoustafa908@gmail.com>
2022-02-21 16:50:25 +0200

    Initial Commit

---
+++ Constant.swift
@@ -0,0 +1,11 @@
+
+import Foundation
+
+class Constamt{
+    static let collectionCellIdId = "category"
+    static let collectionNibName = "categoryCollectionViewCell"
+    static let tableNibName = "RecipeTableViewCell"
+    static let tableCellId = "recipeCell"
+    static let detailSegue = "toDetails"
+    static let recipeDicKey = "recipe"
+}
